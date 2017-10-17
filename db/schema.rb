83ba624ad6ae47d3437933f6aecaafa9b7d216cb
ketwalters <ketwalters@gmail.com>
2017-10-16 22:06:06 -0400

    dropped and recreated database

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20150515175711) do
 
-  create_table "contacts", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
     t.string   "first"
     t.string   "last"
     t.text     "street"
